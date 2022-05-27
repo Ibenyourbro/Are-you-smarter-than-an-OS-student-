9566dc223c5a6ec260ae6f55d857f8beb239be01
ibenyourbro <iben.oneal@gmail.com>
2022-05-27 15:33:20 -0500

    Preparing for merge 3

--- server/db/models/user.model.js
+++ server/db/models/user.model.js
@@ -3,44 +3,52 @@ const findOrCreate = require('mongoose-findorcreate');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 1
-  },
-  name: {
-    type: String,
-    required: true,
-    unique: false,
-    trim: true,
-    minlength: 1
-  },
-  qAttempted: {
-    type: Number,
-    default: 0
-  },
-  qCorrect: {
-    type: Number,
-    default: 0
-  },
-  imageUrl: {
-    type: String,
-    default: 'URL HERE'
-  },
-  wins: {
-    type: Number,
-    default: 0
-  },
-  totalGames: {
-    type: Number,
-    default: 0
-  }
-}, {
-  timestamps: true,
+const dailySchema = new Schema({
+  played: Boolean,
 });
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 1,
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+      minlength: 1,
+    },
+    qAttempted: {
+      type: Number,
+      default: 0,
+    },
+    qCorrect: {
+      type: Number,
+      default: 0,
+    },
+    imageUrl: {
+      type: String,
+      default: 'URL HERE',
+    },
+    wins: {
+      type: Number,
+      default: 0,
+    },
+    totalGames: {
+      type: Number,
+      default: 0,
+    },
+    daily: {},
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.plugin(findOrCreate);
 
